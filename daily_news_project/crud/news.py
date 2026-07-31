da0fdf664ee375ce018d45d75ff926d8b0ec7105
wangguoxi <wangguoxi@darryring.com>
2026-07-31 15:52:31 +0800

    feat(news): 添加新闻详情和相关推荐功能
    - 引入update语句支持新闻浏览量更新
    - 新增get_news_detail函数获取新闻详情
    - 实现increase_news_views函数增加新闻浏览量
    - 添加get_related_news_list函数获取相关推荐新闻
    - 创建/detail接口返回新闻详情和相关推荐列表
    - 集成浏览量自增逻辑和异常处理机制

--- daily_news_project/crud/news.py
+++ daily_news_project/crud/news.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, func
+from sqlalchemy import select, func, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.news import Category, News
@@ -19,3 +19,29 @@ async def get_news_list(db: AsyncSession, category_id: int, offset: int = 0, lim
 async def get_news_count(db: AsyncSession, category_id: int):
     stmt = await db.execute(select(func.count(News.id)).where(News.category_id == category_id))
     return stmt.scalar_one()
+
+async def get_news_detail(db: AsyncSession, news_id: int):
+    stmt = await db.execute(
+        select(News).where(News.id == news_id)
+    )
+    return stmt.scalar_one_or_none()
+
+async def increase_news_views(db: AsyncSession, news_id: int):
+    update_ =  update(News).where(News.id == news_id).values(views=News.views + 1)
+    stmt = await db.execute(update_)
+    return stmt.rowcount
+
+async def get_related_news_list(db: AsyncSession, news_id: int, category_id: int, limit: int = 5):
+    stmt = await db.execute(
+        select(News)
+        .where(
+            News.id != news_id,
+            News.category_id == category_id
+        )
+        .order_by(
+            News.views.desc(),  # 默认升序
+            News.publish_time.desc()
+        )
+        .limit(limit)
+    )
+    return stmt.scalars().all()
--- daily_news_project/routers/news.py
+++ daily_news_project/routers/news.py
@@ -1,6 +1,7 @@
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
 from commons.pages import page_params, page_info
 from crud import news
@@ -31,5 +32,31 @@ async def get_news_list(category_id: int = Query(alias='categoryId', description
             "list": news_list,
             "total": total,
             "hasMore": has_more
+        }
+    }
+
+@router.get('/detail')
+async def get_news_detail(news_id: int = Query(..., alias="id", description="新闻ID"), db: AsyncSession = Depends(get_db)):
+    news_detail = await news.get_news_detail(db, news_id)
+    if not news_detail:
+        raise HTTPException(status_code=500, detail="新闻不存在!")
+    view_res = await news.increase_news_views(db, news_id)
+    if not view_res:
+        raise HTTPException(status_code=500, detail="更新新闻浏览量失败!")
+    related_news = await news.get_related_news_list(db, news_detail.id, news_detail.category_id)
+
+    return {
+        "code": 200,
+        "message": "success",
+        "data": {
+            "id": news_detail.id,
+            "title": news_detail.title,
+            "content": news_detail.content,
+            "image": news_detail.image,
+            "author": news_detail.author,
+            "publishTime": news_detail.publish_time,
+            "categoryId": news_detail.category_id,
+            "views": news_detail.views,
+            "relatedNews": related_news
         }
     }
